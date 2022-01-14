63ba44a6387d2dad54b63fc4b7561c52e4d8bd82
Omid <omidk14@gmail.com>
2022-01-14 16:48:17 +0300

    Add parameters, and documentation

--- app/rest/v1/facebook_marketing_rest.py
+++ app/rest/v1/facebook_marketing_rest.py
@@ -1,36 +1,79 @@
-from flask_restx import Namespace, Resource, cors
+from flask_restx import Namespace, Resource, cors, fields
 from app.services.facebook_marketing_services import FacebookMarketing
 
 facebook_marketing_ns = Namespace(
-    "facebook marketing",
+    "facebook_marketing",
     description="facebook marketing related operations",
     decorators=[cors.crossdomain(origin="*")],
+)
+create_campaign = facebook_marketing_ns.model(
+    "CreateCampaign",
+    {
+        "name": fields.String(required=True, description="The name of campaign"),
+        "objective": fields.String(
+            required=True, description='The objective of objective"'
+        ),
+    },
+)
+
+create_adset = facebook_marketing_ns.model(
+    "CreateAdset",
+    {
+        "name": fields.String(required=True, description="The name"),
+        "budget": fields.String(required=True, description='The daily budget"'),
+        "campaign": fields.String(required=True, description='The campaign"'),
+        "bid": fields.String(required=True, description='The bid"'),
+        "billing_event": fields.String(
+            required=True, description='Chose the suitable billing event"'
+        ),
+        "optimization_goal": fields.String(
+            required=True, description='Chose the right optimization goal"'
+        ),
+        "audience": fields.String(required=True, description='The target audience"'),
+    },
+)
+create_ad = facebook_marketing_ns.model(
+    "CreateAd",
+    {
+        "link_message": fields.String(
+            required=True, description="Creative link message"
+        ),
+        "link": fields.String(required=True, description='Creative link"'),
+        "page_id": fields.String(required=True, description='Page id"'),
+        "name": fields.String(required=True, description='Creative name"'),
+        "image": fields.String(required=True, description='Creative image"'),
+    },
 )
 
 
 @facebook_marketing_ns.route("/create_campaign")
-class HealthCheck(Resource):
+class CreatesCampaign(Resource):
     @facebook_marketing_ns.doc("creates campaign")
-    def post(self):
+    @facebook_marketing_ns.expect(create_campaign)
+    def post(self, name, objective):
         pass
 
 
 @facebook_marketing_ns.route("/create_adset")
-class HealthCheck(Resource):
+class CreateAdset(Resource):
     @facebook_marketing_ns.doc("creates adset")
-    def post(self):
+    @facebook_marketing_ns.expect(create_adset)
+    def post(
+        self, name, budget, campaign, bid, billing_event, optimization_goal, audience
+    ):
         pass
 
 
 @facebook_marketing_ns.route("/create_ad")
-class HealthCheck(Resource):
+class CreateAd(Resource):
     @facebook_marketing_ns.doc("creates ad")
-    def post(self):
+    @facebook_marketing_ns.expect(create_ad)
+    def post(self, link_message, link, page_id, name, image):
         pass
 
 
 @facebook_marketing_ns.route("/get_adset_insigth")
-class HealthCheck(Resource):
+class GetAdsetInsigth(Resource):
     @facebook_marketing_ns.doc("returns a adset insigth")
     def get(self):
         pass
--- app/services/facebook_marketing_services.py
+++ app/services/facebook_marketing_services.py
@@ -1,14 +1,16 @@
 class FacebookMarketing:
     @classmethod
-    def create_campaign(cls):
+    def create_campaign(cls, name, objective):
         pass
 
     @classmethod
-    def create_adset(cls):
+    def create_adset(
+        cls, name, budget, campaign, bid, billing_event, optimization_goal, audience
+    ):
         pass
 
     @classmethod
-    def create_ad(cls):
+    def create_ad(cls, link_message, link, page_id, name, image):
         pass
 
     @classmethod
